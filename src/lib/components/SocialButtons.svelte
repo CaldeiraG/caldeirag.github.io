fe8b2e4faa436e70d72321a7f426ffbe6d8686c5
CaldeiraG <cal@caldeirag.xyz>
2024-03-06 16:31:12 +0000

    added SVGs from SimpleIcons

--- src/lib/components/SocialButtons.svelte
+++ src/lib/components/SocialButtons.svelte
@@ -1,23 +1,26 @@
 <script>
-	import { Mail, Github, Instagram, Twitter, Twitch } from 'lucide-svelte';
+	import { Github, Instagram, Mail, Twitch, Twitter } from 'lucide-svelte';
+
+	import GitHub from '$lib/components/GitHub.svelte';
+
 </script>
 
-<div class="container mx-auto">
+<div class="container mx-auto pt-2">
 	<div class="flex flex-auto gap-4 justify-center items-center pt-2">
 		<!--      <Button variant="default" class="bg-custom text-lg">-->
-		<a href="mailto:cal@caldeirag.xyz"><Mail class="w-4 h-4 mr-2" /> </a>
+		<a href="mailto:cal@caldeirag.xyz"><Mail class="w-6 h-6 mr-2" /> </a>
 		<!--      </Button>-->
 		<!--      <Button variant="default" class="bg-custom text-lg">-->
-		<a href="https://github.com/CaldeiraG"><Github class="w-4 h-4 mr-2" /></a>
+		<a href="https://github.com/CaldeiraG"><GitHub/></a>
 		<!--      </Button>-->
 		<!--      <Button variant="default" class="bg-custom text-lg">-->
-		<a href="https://instagram.com/caldeirag.xyz"><Instagram class="w-4 h-4 mr-2" /></a>
+		<a href="https://instagram.com/caldeirag.xyz"><Instagram/></a>
 		<!--      </Button>-->
 		<!--      <Button variant="default" class="bg-custom text-lg">-->
-		<a href="https://twitter.com/CaldeiraG"><Twitter class="w-4 h-4 mr-2" /></a>
+		<a href="https://twitter.com/CaldeiraG"><Twitter/></a>
 		<!--      </Button>-->
 		<!--      <Button variant="default" class="bg-custom text-lg">-->
-		<a href="https://twitch.tv/CaldeiraG"><Twitch class="w-4 h-4 mr-2" /> </a>
+		<a href="https://twitch.tv/CaldeiraG"><Twitch/> </a>
 		<!--      </Button>-->
 	</div>
 </div>
@@ -29,5 +32,10 @@
         height: 24px;
         stroke-width: 1px;
     }
+
+		svg {
+        width: 24px;
+				height: 24px;
+		}
 
 </style>
